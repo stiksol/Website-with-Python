2950b73cefd00620c713eecc80cef721d8f33aee
Ilya <10it218@gmail.com>
2023-11-26 21:10:08 +0100

    Add a total sum of products to PDF report

--- generatePDF-Invoices/generatePDF-Invoices.py
+++ generatePDF-Invoices/generatePDF-Invoices.py
@@ -34,6 +34,7 @@ for filepath in filepaths:
     pdf.cell(w=35, h=8, txt=columns[3], border=1)
     pdf.cell(w=35, h=8, txt=columns[4], border=1, ln=1)
 
+    total_sum = df["total_price"].sum()
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
@@ -42,6 +43,16 @@ for filepath in filepaths:
         pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=35, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=35, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.cell(w=155, h=8, txt=" ", border=1)
+    pdf.cell(w=35, h=8, txt=str(total_sum), border=1, ln=1)
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}",  ln=1)
+
+    # Add company name and logo
+    pdf.ln(2)
+    pdf.image("../images/pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
